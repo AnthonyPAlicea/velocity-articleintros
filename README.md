8695690ff5ce8c55335b75da368ca7bad58ae42c
Anthony P. Alicea <anthonypalicea@gmail.com>
2014-06-03 18:13:20 -0400

    Initial commit

--- README.md
+++ README.md
@@ -1,4 +1,40 @@
-velocity-articleintros
-======================
+Velocity Article Introductions - v 0.1.0
+========================================
 
-Velocity.JS powered article introductory effects.
+#<a href="http://www.anthonypalicea.com/velocityarticleintros">See the Demo</a>
+
+These simple 7kb article introductions are powered by jQuery and Velocity.JS. They are inspired by <a href="http://tympanus.net/Development/ArticleIntroEffects/">Tympanus Article Intro Effects</a> and explained in this article <a href="http://tympanus.net/codrops/2014/05/22/inspiration-for-article-intro-effects/">here</a>.
+
+Rather than using CSS3 transitions, we use <a href="http://velocityjs.org">Velocity.JS</a> for animations.
+
+On desktop these introductions happen while you scroll. On mobile/touch devices swipe and then continue scrolling. They have both intro and outro animations.
+
+##Required Structure
+The introductions are reusable using the same simple markup and CSS.
+
+Required CSS can be found at 'css/velocityarticleintros.css'
+Javascript can be found at 'Scripts/velocityarticleintros.js'
+
+The HTML structure is simply:
+
+<pre><code>
+<body data-articleintro=''>
+    <header>
+        <div class="header-img">
+            <img />
+        </div>
+        <div>
+            ...header content...
+        </div>
+    </header>
+    <article>
+        <div data-side-width="90%">
+            ...article content...
+        </div>
+    </article>
+</body>
+</code></pre>
+
+The 'data-articleintro' attribute on the body is set to decide which intro to show ('push', 'jam', or 'side').
+
+The 'data-side-width' attribute is only used by the 'side' effect and is to be the width of the article content (note this would need to be redundantly set in CSS).
---
+++ Scripts/velocityarticleintros.js
@@ -0,0 +1,206 @@
+/*
+* Javascript driving the article introductions.
+* jQuery and Velocity.js are the dependencies.
+* For more information see the github repo.
+*
+* Anthony P. Alicea
+* http://www.anthonypalicea.com/velocityarticleintros
+* http://www.github.com/anthonypalicea/velocity-articleintros
+*/
+
+(function() {
+
+    // Set to true once the animation has run, and false when an outro occurs
+    var introAnimationRun = false;
+
+    // Used to mark the initial scroll attempt (if touch device)
+    var isInitialTouchScroll = false;
+
+    // Dummy variable used with setInterval
+    var intervalId = -1;
+
+    // Used only for the 'side' effect
+    var originalArticleWidth = $('article > div').attr('data-side-width'); /* used only for the 'side' effect */
+
+    // Disable touch scrolling (used while intro is visible)
+    function disableTouchScroll() {
+
+                $(document).on("touchmove", "body", function(e) { e.preventDefault(); });
+
+    }
+
+    // Enable touch scrolling (used while intro is invisible)
+    function enableTouchScroll() {
+
+                $(document).off("touchmove", "body");
+
+    }
+
+    // Demo purposes only
+    $("#push").click(function(e) {
+        e.preventDefault();
+
+        $("#chooseEffect a").css("font-weight", "normal");
+        $(this).css("font-weight", "bold");
+        $("body").attr('data-articleintro', 'push');
+    });
+
+    $("#jam").click(function(e) {
+        e.preventDefault();
+
+        $("#chooseEffect a").css("font-weight", "normal");
+        $(this).css("font-weight", "bold");
+        $("body").attr('data-articleintro', 'jam');
+    });
+
+    $("#side").click(function(e) {
+        e.preventDefault();
+
+        $("#chooseEffect a").css("font-weight", "normal");
+        $(this).css("font-weight", "bold");
+        $("body").attr('data-articleintro', 'side');
+    });
+    // End demo purposes only
+
+    // Initially disable touch
+    disableTouchScroll();
+
+    // The intro animations are defined here
+    var introAnimation = function() {
+
+        var currentScrollTop = $(this).scrollTop();
+
+        // INTRO
+        // If we have scrolled, or attempted to on a touch device
+        if (currentScrollTop > 5 || (isInitialTouchScroll)) {
+
+            // If the animation isn't running
+            if (!introAnimationRun)
+            {
+                // initiate intro animation
+                introAnimationRun = true;
+
+                // don't bother running setInterval while the animation is running
+                window.clearInterval(intervalId);
+
+                var articleIntro = $("body").attr('data-articleintro');
+                
+                if (!articleIntro || articleIntro == "")
+                {
+                    alert("Missing 'data-articleintro' attribute from the <body> tag");
+                }
+
+                // push
+                if (articleIntro == 'push')
+                {
+                    $('div.header-img').velocity({ translateY: "-80%", opacity: 0, scale: 0.9 }, { duration: 800, queue: false, easing: [0.7,0,0.3,1] });
+
+                }
+
+                // jam
+                if (articleIntro == 'jam')
+                {
+                    $('div.header-img').velocity({ translateY: "-75%", opacity: 0.3 }, { duration: 500, queue: false, easing: "ease-out" });
+                }
+
+                // side
+                if (articleIntro == 'side')
+                {
+                    $('article > div').css("width", "50%");
+                    $('article > div').velocity({ translateX: "-25%" }, { duration: 0 });
+                    $('div.header-img').velocity({ translateX: "70%" }, { duration: 500, easing: [0.7,0,0.3,1] });
+
+                    if (!originalArticleWidth || originalArticleWidth == "")
+                    {
+                        alert('To use the side effect you need to set a data-side-width attribute on the article <div>.\n\nOtherwise we don\'t know what width to return the content to later.');
+                    }
+                }
+
+                // bring in the article contents
+                $('article').velocity({ translateY: "-200px", opacity: 1 }, { duration: 500, queue: false, delay: 200, 
+                complete: function() {                     
+                    if (isInitialTouchScroll) 
+                    {
+                        // scroll just a bit to not trigger the outro animation
+                        $(document).scrollTop(2); 
+                    } 
+
+                    // reenable touch scrolling once the article contents is visible
+                    enableTouchScroll(); 
+                    isInitialTouchScroll = false; 
+                    } });
+
+                // for demo purposes only: hide the chooser
+                $("#chooseEffect").velocity({ opacity: 0 });
+
+                // start the interval check again
+                intervalId = setInterval(introAnimation, 100);
+            }
+
+        }
+        // OUTRO
+        // if we have moved back to the top
+        else if (currentScrollTop <= 1) {
+
+            // and the animation has been run
+            if (introAnimationRun)
+            {
+                // don't bother running setInterval while the animation is running
+                window.clearInterval(intervalId);
+
+                // initiate outro animation of article contents
+                $('article').velocity({ translateY: "200px", opacity: 0 }, { duration: 400, queue: false, easing: "ease-out" });
+
+                var articleIntro = $("body").attr('data-articleintro');
+
+                // push
+                if (articleIntro == 'push')
+                {
+                    $('div.header-img').velocity({ translateY: "0", opacity: 1, scale: 1 }, { duration: 700, queue: false, easing: [0.7,0,0.3,1] });
+                }
+
+                // jam
+                if (articleIntro == 'jam')
+                {
+                    $('div.header-img').velocity({ translateX: "0", translateY: "0", scale: 1, opacity: 1 }, { duration: 700, queue: false, easing: [0.7,0,0.3,1] });
+                }
+
+                // side
+                if (articleIntro == 'side')
+                {
+                    $('div.header-img').velocity({ translateX: "0" }, { duration: 700, delay: 200, queue: false, easing: [0.7,0,0.3,1], complete: function() { $('article > div').css("width", originalArticleWidth); } });
+                    $('article > div').velocity({ translateX: "0" }, { duration: 800, queue: false });
+                }
+
+                // mark the animation as having not been run since we did the outro
+                introAnimationRun = false;
+
+                // for demo purposes only: show the chooser
+                $("#chooseEffect").velocity({ opacity: 1});
+
+                // start the interval again
+                intervalId = setInterval(introAnimation, 100);
+
+                // disable touch scrolling
+                disableTouchScroll();
+            }
+
+        }
+
+    }
+
+    // we choose to check the scroll position at intervals rather than rely on the scroll event
+    intervalId = setInterval(introAnimation, 100);
+
+    // capture touch scrolling
+    $(document).on("touchmove", "body > header", function() { 
+
+        // run the intro if we are at the top and it hasn't been run
+        if ($(window).scrollTop() == 0 && !isInitialTouchScroll && !introAnimationRun)
+        {
+            isInitialTouchScroll = true;
+            introAnimation();
+        }
+
+    });
+})();
